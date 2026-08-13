82c3cd966a451e60fb3283c3c7f8424aadf03e30
Priyanshu792 <priyanshuhoney2002@gmail.com>
2026-08-13 20:34:44 +0530

    code refactor complete settings resolution custom resolution works normal

--- script/settings.gd
+++ script/settings.gd
@@ -1,6 +1,5 @@
 extends Control
 
-#@onready var option_button: OptionButton = $VBoxContainer/OptionButton
 @onready var option_button: OptionButton = $VBoxContainer/HBoxContainer/VBoxContainer2/OptionButton
 @onready var option_button_2: OptionButton = $VBoxContainer/HBoxContainer/VBoxContainer2/OptionButton2
 @onready var option_button_3: OptionButton = $VBoxContainer/HBoxContainer/VBoxContainer2/OptionButton3
@@ -9,28 +8,8 @@ extends Control
 @onready var custom_res_val_1: LineEdit = $VBoxContainer/HBoxContainer/VBoxContainer2/custom_res/custom_res_val1
 @onready var custom_res_val_2: LineEdit = $VBoxContainer/HBoxContainer/VBoxContainer2/custom_res/custom_res_val2
 
-#var custom_size:Vector2i
+
 var current_size = DisplayServer.window_get_size()
-
-
-#const RESOLUTIONS: Array[Vector2i] = [
-	#Vector2i(504, 504),
-	#Vector2i(320, 180),
-	#Vector2i(640, 360),
-	#Vector2i(960, 540),
-	#Vector2i(1280, 720),
-	#Vector2i(1920, 1032),
-#]
-#var RESOLUTIONS: Array[String] = [
-	#"504X504",
-	#"320X180",
-	#"640X360",
-	#"960X540",
-	#"1280X720",
-	#"1920X1032",
-	#"CUSTOM"
-#]
-
 var RESOLUTIONS = WindowsManager.RESOLUTIONS
 
 func _ready():
@@ -49,31 +28,31 @@ func _ready():
 	option_button.add_item("Bottom Left")
 	option_button.add_item("Bottom Center")
 	option_button.add_item("Bottom Right")
-
-	# Show the currently saved selection
+	
 	option_button.select(WindowsManager.anchor)
-
-	#option_button.item_selected.connect(_on_option_selected)
-	
 	option_button_2.clear()
-	
-	# Loop through the list to add text items (e.g., "1920 x 1080")
 	for res in RESOLUTIONS:
-		#option_button_2.add_item(str(res.x) + " x " + str(res.y))
 		option_button_2.add_item(res)
-	
-	#var current_size = DisplayServer.window_get_size()
+		
+	option_button_2.select(WindowsManager.resolution_idx)
+	if RESOLUTIONS[WindowsManager.resolution_idx] == "CUSTOM":
+		custom_res.show()
+
+		custom_res_val_1.text = str(WindowsManager.custom_size.x)
+		custom_res_val_2.text = str(WindowsManager.custom_size.y)
+	else:
+		custom_res.hide()
+
+
 	var current_size_string = "%dX%d" % [current_size.x, current_size.y]
-	#print("%d"%current_size.x)
 	var current_index = RESOLUTIONS.find(current_size_string)
 	if current_index != -1:
 		option_button_2.select(current_index)
-	GameEvents.resolution_changed.connect(custom_resolution_applied)
+	#GameEvents.resolution_changed.connect(custom_resolution_applied)
 
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
-	#print(get_parent().get_node("settings"))
 	get_parent().get_node("settings").hide()
 	if get_parent().get_node("SettingsButton"):
 		get_parent().get_node("SettingsButton").show()
@@ -83,22 +62,27 @@ func _on_resume_button_pressed() -> void:
 
 func _on_option_button_item_selected(index: int) -> void:
 	WindowsManager.set_anchor(index)
-	pass # Replace with function body.
+	pass 
 
 
 func _on_option_button_2_item_selected(index: int) -> void:
 	if RESOLUTIONS[index] == "CUSTOM":
 		custom_res.show()
+		
+		custom_res_val_1.text = str(WindowsManager.custom_size.x)
+		custom_res_val_2.text = str(WindowsManager.custom_size.y)
+		
 		return
+	custom_res.hide()
 	WindowsManager.apply_resolution(index)
 	pass
 
-func _on_custom_res_val_1_text_submitted(new_text: String) -> void:
+func _on_custom_res_val_1_text_submitted(_new_text: String) -> void:
 	apply_custom_res()
 	pass
 
 
-func _on_custom_res_val_2_text_submitted(new_text: String) -> void:
+func _on_custom_res_val_2_text_submitted(_new_text: String) -> void:
 	apply_custom_res()
 	
 	pass
@@ -107,26 +91,30 @@ func apply_custom_res():
 	if custom_res_val_1.text.is_empty() or custom_res_val_2.text.is_empty():
 		return
 	WindowsManager.apply_custom_resolution(custom_res_val_1.text,custom_res_val_2.text)
+	
+	var custom_index := RESOLUTIONS.find("CUSTOM")
+	if custom_index != -1:
+		option_button_2.select(custom_index)
+	#custom_res.hide()
 	pass
 
-func custom_resolution_applied(custom_size_1,custom_size_2):
-	var resolution_string := "%dX%d" % [custom_size_1, custom_size_2]
-	if not RESOLUTIONS.has(resolution_string):
-		RESOLUTIONS.insert(RESOLUTIONS.size() - 1, resolution_string)
-		option_button_2.add_item(resolution_string)
-
-	option_button_2.select(RESOLUTIONS.find(resolution_string))
-	custom_res.hide()
-	pass
+#func custom_resolution_applied(custom_size_1,custom_size_2):
+	#var resolution_string := "%dX%d" % [custom_size_1, custom_size_2]
+	#if not RESOLUTIONS.has(resolution_string):
+		#RESOLUTIONS.insert(RESOLUTIONS.size() - 1, resolution_string)
+		#option_button_2.add_item(resolution_string)
+#
+	#
+	#custom_res.hide()
+	#option_button_2.select(RESOLUTIONS.find(resolution_string))
+	#pass
 
 func _on_option_button_3_item_selected(index: int) -> void:
 	WindowsManager.set_current_screen(index)
-	pass # Replace with function body.
+	pass
 
 
 func _on_debug_button_pressed() -> void:
 	var player = get_tree().get_first_node_in_group("player")
-	#var size = DisplayServer.window_get_size()
 	player.position = size/2.0
-	#print(size)
-	pass # Replace with function body.
+	pass
--- script/windows_manager.gd
+++ script/windows_manager.gd
@@ -42,28 +42,17 @@ func _ready():
 func apply_window_settings():
 	var screen := DisplayServer.screen_get_usable_rect()
 	var window_size := DisplayServer.window_get_size()
-
 	var pos := Vector2i.ZERO
-
 	match anchor:
 		WindowAnchor.TOP_LEFT:pos = Vector2i(0, 0)
-
 		WindowAnchor.TOP_CENTER:pos = Vector2i((screen.size.x - window_size.x) / 2,0)
-
 		WindowAnchor.TOP_RIGHT:pos = Vector2i(screen.size.x - window_size.x,0)
-
 		WindowAnchor.CENTER_LEFT:pos = Vector2i(0,(screen.size.y - window_size.y) / 2)
-
 		WindowAnchor.CENTER:pos = Vector2i((screen.size.x - window_size.x) / 2,(screen.size.y - window_size.y) / 2)
-
 		WindowAnchor.CENTER_RIGHT:pos = Vector2i(screen.size.x - window_size.x,(screen.size.y - window_size.y) / 2)
-
 		WindowAnchor.BOTTOM_LEFT:pos = Vector2i(0,screen.size.y - window_size.y)
-
 		WindowAnchor.BOTTOM_CENTER:pos = Vector2i((screen.size.x - window_size.x) / 2,screen.size.y - window_size.y)
-
 		WindowAnchor.BOTTOM_RIGHT:pos = Vector2i(screen.size.x - window_size.x,screen.size.y - window_size.y)
-
 	DisplayServer.window_set_position(screen.position + pos)
 
 func set_anchor(new_anchor: WindowAnchor):
@@ -75,42 +64,40 @@ func save_settings():
 	config.set_value("window", "anchor", anchor)
 	config.set_value("window", "resolution", resolution_idx)
 	config.set_value("window", "screen", screen_idx)
+	config.set_value("window", "custom_width", custom_size.x)
+	config.set_value("window", "custom_height", custom_size.y)
+
 	config.save(SETTINGS_PATH)
 
 func load_settings():
 	var config := ConfigFile.new()
-
 	if config.load(SETTINGS_PATH) == OK:
 		anchor = config.get_value("window","anchor",WindowAnchor.BOTTOM_RIGHT)
 		screen_idx = config.get_value("window","screen")
 		resolution_idx = config.get_value("window","resolution")
+		var custom_width = config.get_value("window","custom_width",604)
+		var custom_height = config.get_value("window","custom_height",604)
+		custom_size = Vector2i(custom_width,custom_height)
 
 func get_game_state():
-	
 	pass
 
 func update_walls(size:Vector2):
 	var walls = get_tree().get_nodes_in_group("walls")
-	#print(walls)
 	for wall in walls:
 		if !(wall is CollisionShape2D):
 			continue
 		match wall.name:
 			"Top":
 				wall.position = Vector2(size.x / 2, 0)
-				#print("Top ", wall.position)
 			"Bottom":
 				wall.position = Vector2(size.x / 2, size.y)
-				#print("Bottom ", wall.position)
 			"Left":
 				wall.position = Vector2(0, size.y / 2)
-				#print("Left ", wall.position)
 			"Right":
 				wall.position = Vector2(size.x, size.y / 2)
-				#print("Right ", wall.position)
-#	
-	load_settings()
-	apply_window_settings()
+	#load_settings()
+	#apply_window_settings()
 
 func set_current_screen(idx:int):
 	screen_idx = idx
@@ -135,7 +122,8 @@ func apply_custom_resolution(custom_res_val_1,custom_res_val_2):
 func set_resolution():
 	var res := RESOLUTIONS[resolution_idx]
 	if res == "CUSTOM":
-		#custom_res.show()
+		DisplayServer.window_set_size(custom_size)
+		GameEvents.resolution_changed.emit(custom_size.x,custom_size.y)
 		return
 	var res_split := res.split("X")
 	var size := Vector2i(int(res_split[0]),int(res_split[1]))
@@ -146,20 +134,17 @@ func set_resolution():
 func set_custom_resolution(custom_res_val_1,custom_res_val_2):
 	var width := int(custom_res_val_1)
 	var height := int(custom_res_val_2)
-	
 	width = clampi(width, 320, 1920)
 	height = clampi(height, 180, 1080)
-	
 	if width <= 0 or height <= 0:
 		return
-
 	custom_size = Vector2i(width, height)
+	
+	resolution_idx = RESOLUTIONS.find("CUSTOM")
 
 	DisplayServer.window_set_size(custom_size)
 	GameEvents.resolution_changed.emit(custom_size.x, custom_size.y)
-	
-	var resolution_string := "%dX%d" % [custom_size.x, custom_size.y]
-
-	if not RESOLUTIONS.has(resolution_string):
-		RESOLUTIONS.insert(RESOLUTIONS.size() - 1, resolution_string)
+	#var resolution_string := "%dX%d" % [custom_size.x, custom_size.y]
+	#if not RESOLUTIONS.has(resolution_string):
+		#RESOLUTIONS.insert(RESOLUTIONS.size() - 1, resolution_string)
 	pass
